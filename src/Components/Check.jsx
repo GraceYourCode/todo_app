fd040d4dda01fb0d6eaf9402a2256d708744a8d8
GraceYourCode <freshmannskuls@gmail.com>
2023-12-28 01:07:17 -0800

    working on a todo app

--- src/Components/Check.jsx
+++ src/Components/Check.jsx
@@ -1,6 +1,6 @@
 import { useRef } from "react"
 
-const Check = ({dark_mode}) => {
+const Check = ({dark_mode, check_Control}) => {
   const checker = useRef();
   const checkbox = useRef();
 
@@ -9,14 +9,7 @@ const Check = ({dark_mode}) => {
 
   return (
     <>
-      <input type="checkbox" ref={checkbox} onClick={()=>{
-        /*
-        this function checks if the checkbox is checked or not and then
-        displays the next tag(SVG tag) which is hidden by default
-        */
-       checkbox.current.checked?checker.current.style.display = "block" : checker.current.style.display = "none";
-       console.log(checkbox.current.checked)
-      }}
+      <input type="checkbox" ref={checkbox} onClick={()=>check_Control(checkbox, checker)}
         className={`appearance-none w-5 h-5 rounded-full ${dark_mode?"border-dark_Grey_Blue":"border-very_Light_Grey_Blue"}
       border-2 checked:bg-gradient-to-r from-first_Grad to-second_Grad checked:border-none cursor-pointer`} />
       <svg ref={checker} xmlns="http://www.w3.org/2000/svg" width="11" height="9"
--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -1,12 +1,19 @@
 import Check from "./Check"
 
 const Task = ({dark_Mode}) => {
+  const checker_Event_Handler = (checkbox, checker) => {
+    /*
+        this function checks if the checkbox is checked or not and then
+        displays the next tag(SVG tag) which is hidden by default
+        */
+        checkbox.current.checked?checker.current.style.display = "block" : checker.current.style.display = "none";
+        console.log(checkbox.current.checked)
+  }
 
-  
   return (
     <div className="flex px-6 py-4 gap-5 items-center border-b-1 border-b-dark_Grey_Blue">
-      <Check dark_mode={dark_Mode} />
-      <p className={`w-4/5 text-sm ${dark_Mode?"text-light_Grey_Blue":"text-light_Mode_Very_Dark_Grey_Blue"} body`}>Complete online javascript course</p>
+      <Check dark_mode={dark_Mode} check_Control={()=>checker_Event_Handler()} />
+      <p className={`w-4/5 text-sm ${dark_Mode?"text-light_Grey_Blue":"text-light_Mode_Very_Dark_Grey_Blue"}`}>Complete online javascript course</p>
       <button className="invisible">
       <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fillRule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
       </button>
