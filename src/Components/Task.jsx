38417c29e8c83ddfd7c362773da4f9903464ed13
GraceYourCode <freshmannskuls@gmail.com>
2024-01-02 01:06:23 -0800

    imputing all functionalities

--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -37,7 +37,7 @@ const Task = ({ dark_Mode, task, delete_Task }) => {
       <p className={`w-4/5 text-sm ${dark_Mode ? (task.completed ? "text-dark_Grey_Blue" : "text-light_Grey_Blue") :
         (task.completed ? "text-light_Mode_Dark_Grey_Blue" : "text-light_Mode_Very_Dark_Grey_Blue")} 
       ${task.completed && "line-through"}`}>{task.task}</p>
-      <button className="hidden" ref={del_Btn} onClick={() => delete_Task(task.id)}>
+      <button className="portrait:block hidden" ref={del_Btn} onClick={() => delete_Task(task.id)}>
         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fillRule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z" /></svg>
       </button>
     </div>
