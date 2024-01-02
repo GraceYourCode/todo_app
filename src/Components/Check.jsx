e8092fb4cbaff31f5bdb2171c68a01c3cef3f748
GraceYourCode <freshmannskuls@gmail.com>
2024-01-02 00:59:07 -0800

    imputing all functionalities

--- src/Components/Check.jsx
+++ src/Components/Check.jsx
@@ -10,8 +10,8 @@ const Check = ({dark_mode, check_Control, task, completed}) => {
     <>
       <input type="checkbox" ref={checkbox} onClick={()=>check_Control(checkbox, checker, task)} checked={completed}
         className={`appearance-none w-4 h-4 md:w-5 md:h-5 rounded-full ${dark_mode?"border-very_Dark_Grey_Blue":"border-very_Light_Grey_Blue"}
-      border-1 hover:border-l-first_Grad hover:border-b-first_Grad hover:border-r-second_Grad hover:border-t-second_Grad 
-      checked:bg-gradient-to-r from-first_Grad to-second_Grad checked:border-none cursor-pointer outline-none`} />
+      border-1 xl:hover:border-l-first_Grad xl:hover:border-b-first_Grad xl:hover:border-r-second_Grad xl:hover:border-t-second_Grad 
+      checked:bg-gradient-to-r from-first_Grad to-second_Grad checked:border-none cursor-pointer outline-none ml-0.5 md:ml-1.5`} />
       <svg ref={checker} xmlns="http://www.w3.org/2000/svg" width="11" height="9" style={{marginLeft: "5px"}}
       className={`absolute ${completed ? "block" : "hidden"}`}
       ><path fill="none" stroke="#FFF" strokeWidth="2" d="M1 4.304L3.696 7l6-6"/></svg>
