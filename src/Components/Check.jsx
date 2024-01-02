98e95105ab9c1d16bf84fc012a13b63a25f4b7e8
GraceYourCode <freshmannskuls@gmail.com>
2024-01-02 01:11:46 -0800

    imputing all functionalities

--- src/Components/Check.jsx
+++ src/Components/Check.jsx
@@ -12,8 +12,8 @@ const Check = ({dark_mode, check_Control, task, completed}) => {
         className={`appearance-none w-4 h-4 md:w-5 md:h-5 rounded-full ${dark_mode?"border-very_Dark_Grey_Blue":"border-very_Light_Grey_Blue"}
       border-1 xl:hover:border-l-first_Grad xl:hover:border-b-first_Grad xl:hover:border-r-second_Grad xl:hover:border-t-second_Grad 
       checked:bg-gradient-to-r from-first_Grad to-second_Grad checked:border-none cursor-pointer outline-none`} />
-      <svg ref={checker} xmlns="http://www.w3.org/2000/svg" width="11" height="9" style={{marginLeft: "5px"}}
-      className={`absolute ${completed ? "block" : "hidden"} ml-1 md:ml-1`}
+      <svg ref={checker} xmlns="http://www.w3.org/2000/svg" width="11" height="9"
+      className={`absolute ${completed ? "block" : "hidden"} ml-0.5 md:ml-1`}
       ><path fill="none" stroke="#FFF" strokeWidth="2" d="M1 4.304L3.696 7l6-6"/></svg>
     </>
   )
