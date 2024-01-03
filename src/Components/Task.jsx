2395ac36a123ee06f1a1c3a347f59cfb130322c5
GraceYourCode <freshmannskuls@gmail.com>
2024-01-03 04:41:22 -0800

    imputing all functionalities

--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -1,10 +1,12 @@
 import { useContext, useRef, useState } from "react";
 import Check from "./Check"
 import { tasks } from "../Store";
+import { useOrientation } from "react-use";
 
 const Task = ({ dark_Mode, task, delete_Task }) => {
   let { allTasks, setAllTasks } = useContext(tasks);
   const del_Btn = useRef();
+  const { type } = useOrientation();
 
   const checker_Event_Handler = (checkbox, checker) => {
     /*
@@ -12,34 +14,34 @@ const Task = ({ dark_Mode, task, delete_Task }) => {
         displays the next tag(SVG tag) which is hidden by default
         and also strike-through the todo text
         */
-      let completed_Event = allTasks.filter(aTask=> aTask.id == task.id)[0];
+    let completed_Event = allTasks.filter(aTask => aTask.id == task.id)[0];
 
     if (checkbox.current.checked) {
       completed_Event.completed = true;
-      let other_Tasks = allTasks.filter(theTasks=> theTasks.id != completed_Event.id)
+      let other_Tasks = allTasks.filter(theTasks => theTasks.id != completed_Event.id)
       setAllTasks([...other_Tasks, completed_Event]);
       localStorage.setItem("allTasks", JSON.stringify(allTasks));
     } else {
       completed_Event.completed = false;
-      let other_Tasks = allTasks.filter(theTasks=> theTasks.id != completed_Event.id)
+      let other_Tasks = allTasks.filter(theTasks => theTasks.id != completed_Event.id)
       setAllTasks([...other_Tasks, completed_Event]);
       localStorage.setItem("allTasks", JSON.stringify(allTasks));
     }
   }
 
   const show_Del_Btn = () => del_Btn.current.style.display = "block";
-  const cancel_Del_Btn = () => del_Btn.current.style.display = "none"
+  const cancel_Del_Btn = () => del_Btn.current.style.display = "none";
 
   return (
-    <div className={`flex ${dark_Mode ? "bg-desaturated_Blue" : "bg-light_Grey"} px-3 md:px-4 lg:px-6 py-4 gap-3.5 lg:gap-5 items-center mb-0.5`}
-      onMouseOver={() => show_Del_Btn()} onMouseOut={() => cancel_Del_Btn()} draggable>
+    <div className={`flex ${dark_Mode ? "bg-desaturated_Blue" : "bg-light_Grey"} px-3 md:px-4 lg:px-6 py-4 gap-3.5 lg:gap-5 justify-between items-center mb-0.5`}
+      onMouseOver={() => show_Del_Btn()} onMouseOut={() => cancel_Del_Btn()}>
       <Check dark_mode={dark_Mode} check_Control={checker_Event_Handler} completed={task.completed} />
-      <p className={`w-4/5 text-xs md:text-sm ${dark_Mode ? (task.completed ? "text-dark_Grey_Blue" : "text-light_Grey_Blue") :
+      <p className={`w-full text-xs md:text-sm ${dark_Mode ? (task.completed ? "text-dark_Grey_Blue" : "text-light_Grey_Blue") :
         (task.completed ? "text-light_Mode_Dark_Grey_Blue" : "text-light_Mode_Very_Dark_Grey_Blue")} 
       ${task.completed && "line-through"}`}>{task.task}</p>
-      <button className="hidden" ref={del_Btn} onClick={() => delete_Task(task.id)}>
+      {type == "landscape-primary" && <button className="hidden" ref={del_Btn} onClick={() => delete_Task(task.id)}>
         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fillRule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z" /></svg>
-      </button>
+      </button>}
       <button className="portrait:block hidden" onClick={() => delete_Task(task.id)}>
         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fillRule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z" /></svg>
       </button>
