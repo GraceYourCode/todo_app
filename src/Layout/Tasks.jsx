dc054724b5d25cde227571ac078523941c55d633
GraceYourCode <freshmannskuls@gmail.com>
2024-01-03 06:25:37 -0800

    imputing all functionalities

--- src/Layout/Tasks.jsx
+++ src/Layout/Tasks.jsx
@@ -17,18 +17,26 @@ const Tasks = ({ dark_Mode }) => {
     localStorage.setItem("allTasks", JSON.stringify(allTasks));
     filter.active && setActiveTasks(activeTasks = activeTasks.filter(task => task.id != id));
     filter.completed && setCompletedTasks(completedTasks = completedTasks.filter(task => task.id != id));
-    console.log(allTasks.length == 0)
   }
 
   return (
     <div className={`rounded`}>
-      {filter.all ? allTasks.map(task => (<>
-        <Task dark_Mode={dark_Mode} task={task} key={task.id} delete_Task={delete_Task} /></>
-      )) : filter.active ? activeTasks.map(task => (
-        <Task dark_Mode={dark_Mode} task={task} key={task.id} delete_Task={delete_Task} />
-      )) : completedTasks.map(task => (
-        <Task dark_Mode={dark_Mode} task={task} key={task.id} delete_Task={delete_Task} />
-      ))
+      {filter.all ? allTasks.length == 0 ? <p className={`text-center text-xs md:text-sm px-3 md:px-4 lg:px-6 py-4 mb-0.5
+      ${dark_Mode ? "text-light_Grey_Blue bg-desaturated_Blue" : "text-light_Mode_Very_Dark_Grey_Blue bg-light_Grey"}`}>
+        No tasks to show</p> :
+        allTasks.map(task => (<>
+          <Task dark_Mode={dark_Mode} task={task} key={task.id} delete_Task={delete_Task} /></>
+        )) : filter.active ? activeTasks.length == 0 ? <p className={`text-center text-xs md:text-sm px-3 md:px-4 lg:px-6 py-4 mb-0.5
+      ${dark_Mode ? "text-light_Grey_Blue bg-desaturated_Blue" : "text-light_Mode_Very_Dark_Grey_Blue bg-light_Grey"}`}>
+          No tasks to show</p> :
+          activeTasks.map(task => (
+            <Task dark_Mode={dark_Mode} task={task} key={task.id} delete_Task={delete_Task} />
+          )) : completedTasks.length == 0 ? <p className={`text-center text-xs md:text-sm px-3 md:px-4 lg:px-6 py-4 mb-0.5
+          ${dark_Mode ? "text-light_Grey_Blue bg-desaturated_Blue" : "text-light_Mode_Very_Dark_Grey_Blue bg-light_Grey"}`}>
+            No tasks to show</p> :
+        completedTasks.map(task => (
+          <Task dark_Mode={dark_Mode} task={task} key={task.id} delete_Task={delete_Task} />
+        ))
       }
       <Footer dark_Mode={dark_Mode} items={
         filter.all ? allTasks.length : filter.active ? activeTasks.length : completedTasks.length
